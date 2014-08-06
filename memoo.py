06879c1d5ad7a03e6cde43f9274b59760a612dd4
Lucas Possatti <lucas_possatti@hotmail.com>
2014-08-06 20:15:21 -0300

    Criação do programa principal

---
+++ memoo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Programa principal para a conversão de máquinas Mealy e Moore.
+
+Autor: Lucas Possatti
+'''
+
+import sexpr
+import converter
+import sys
+
+# Lê a S-Expression fornecida através do stdin.
+sexp = sys.stdin.read()
+
+# Imprime a máquina lida.
+print('>> Original machine:\n%s\n' % sexp)
+
+# Faz o parsing.
+machine = sexpr.parse_sexp(sexp)
+
+# Placeholder para a conversão da máquina.
+converted_machine = []
+
+# Detecta a máquina e faz a conversão.
+if machine[0] == 'moore':
+	converted_machine = converter.moore_to_mealy(machine)
+elif machine[0] == 'mealy':
+	converted_machine = converter.moore_to_mealy(machine)
+else:
+	raise "A máquina indicada não pode ser reconhecida."
+
+
+# Joga a máquina convertida, de volta para uma S-Expression.
+final_sexp = sexpr.print_sexp(converted_machine)
+
+# Imprime a máquina já convertida.
+print(">> Converted machine:\n%s\n" % final_sexp)
