549e43812809ccb3363232070c35a0c87162db77
Lucas Possatti <lucas_possatti@hotmail.com>
2014-08-09 15:22:30 -0300

    Tratamento das opções da linha de comando

--- memoo.py
+++ memoo.py
@@ -6,16 +6,36 @@ Autor: Lucas Possatti
 
 import sexp
 import converter
-import sys
+from sys import stdin
+from optparse import OptionParser
 
-# Lê a S-Expression fornecida através do stdin.
-sexp = sys.stdin.read()
+# Define a interface da linha de comando.
+opt_parse = OptionParser()
+opt_parse.add_option("-i", "--input", dest="inputfile_name",
+                 help="File to be read which contains the original machine for conversion. `--` for reading from standard input (default).",
+                 metavar="FILE", default="--")
+opt_parse.add_option("-o", "--output", dest="outputfile_name",
+                 help="File where to write the converted machine. `--` for writing to standard output (default).",
+                 metavar="FILE", default="--")
 
-# Imprime a máquina lida.
-print('>> Original machine:\n%s\n' % sexp)
+# Captura as opções e os argumentos recebidos na linha de comando.
+(options, args) = opt_parse.parse_args()
+
+# Lê a maquina que deve ser convertida.
+raw_sexp = ""
+if options.inputfile_name == "--":
+	# Lê a S-Expression fornecida através do standard input.
+	raw_sexp = stdin.read()
+else:
+	# Lê a S-Expression do arquivo de entrada.
+	with open(options.inputfile_name, 'r') as input_file:
+		raw_sexp = input_file.read()
+
+# Imprime a máquina lida (DEBUG).
+print('>> Original machine:\n%s\n' % raw_sexp)
 
 # Faz o parsing.
-machine = sexpr.parse_sexp(sexp)
+machine = sexp.parse_sexp(raw_sexp)
 
 # Placeholder para a conversão da máquina.
 converted_machine = []
@@ -28,9 +48,16 @@ elif machine[0] == 'mealy':
 else:
 	raise "A máquina indicada não pode ser reconhecida."
 
-
 # Joga a máquina convertida, de volta para uma S-Expression.
-final_sexp = sexpr.print_sexp(converted_machine)
+final_sexp = sexp.print_sexp(converted_machine) + "\n"
 
-# Imprime a máquina já convertida.
-print(">> Converted machine:\n%s\n" % final_sexp)
+# Escreve no arquivo ou imprime a máquina já convertida.
+if options.outputfile_name == "--":
+	# Imprime o resultado no standard output.
+	print(final_sexp)
+	#writer.print_machine(converted_machine)
+else:
+	# Escreve o resultado em um arquivo.
+	with open(options.outputfile_name, 'w') as f:
+		f.write(final_sexp)
+	#writer.write_machine_to_file(converted_machine, options.outputfile_name)
